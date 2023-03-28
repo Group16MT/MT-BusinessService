55f45e5cf234111ed7f352bc671d572eb9c6d324
Pawan Rajoriya <prajoriya@dxc.com>
2023-03-28 22:06:45 +0530

    Transaction and History

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import { Transaction } from './transaction';
+import { from } from 'form-data';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +17,8 @@ export class EmployeeService {
   getEmployeesList(): Observable<Employee[]>{
     return this.httpClient.get<Employee[]>(`${this.baseURL}`);
   }
-  getTransactionHistory(startDate : string ,endDate : string): Observable<Employee[]>{
-    return this.httpClient.get<Employee[]>(`${this.baseURL +
+  getTransactionHistory(startDate : string ,endDate : string): Observable<Transaction[]>{
+    return this.httpClient.get<Transaction[]>(`${this.baseURL +
       `/transactionaDetails?` +
       `startDate=${startDate}&` +
       `endDate=${endDate}`
@@ -36,6 +38,17 @@ export class EmployeeService {
 
   updateEmployee(id: number, employee: Employee, selectedOption:number): Observable<Object>{
     return this.httpClient.put(`${this.baseURL}/${id}?`+`selectedOption=${selectedOption}`, employee);
+  }
+
+  transaction(fromAccountNum:number, toAccountNum:number, amount: number): Observable<Object>{
+    let transaction = new Transaction();
+    transaction.dateTime = new Date().toDateString();
+    transaction.fromAccountNum = fromAccountNum;
+    transaction.toAccountNum = toAccountNum;
+    transaction.amount = amount;
+    console.log(transaction)
+    return this.httpClient.post(`${this.baseURL}/transactionaDetails`, transaction);
+
   }
 
   deleteEmployee(id: number): Observable<Object>{
